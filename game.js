aeeea6532b8b01a27a62045b6855659d8e174b14
Jayesh121005 <jayeshreddy569@gmail.com>
2026-03-18 13:53:55 +0530

    Update game.js
    Added score feature

--- game.js
+++ game.js
@@ -1,4 +1,3 @@
-
 var buttonColours = ["red", "blue", "green", "yellow"];
 
 var gamePattern = [];
@@ -6,8 +5,10 @@ var userClickedPattern = [];
 
 var started = false;
 var level = 0;
+var highScore = 0;
 
-$(document).keypress(function() {
+// Start game on click (works with sound policies)
+$(document).on("click", function () {
   if (!started) {
     $("#level-title").text("Level " + level);
     nextSequence();
@@ -15,61 +16,73 @@ $(document).keypress(function() {
   }
 });
 
-$(".btn").click(function() {
-
+$(".btn").click(function () {
   var userChosenColour = $(this).attr("id");
   userClickedPattern.push(userChosenColour);
 
   playSound(userChosenColour);
   animatePress(userChosenColour);
 
-  checkAnswer(userClickedPattern.length-1);
+  checkAnswer(userClickedPattern.length - 1);
 });
 
 function checkAnswer(currentLevel) {
 
-    if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
-      if (userClickedPattern.length === gamePattern.length){
-        setTimeout(function () {
-          nextSequence();
-        }, 1000);
-      }
-    } else {
-      playSound("wrong");
-      $("body").addClass("game-over");
-      $("#level-title").text("Game Over, Press Any Key to Restart");
+  if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
 
+    if (userClickedPattern.length === gamePattern.length) {
       setTimeout(function () {
-        $("body").removeClass("game-over");
-      }, 200);
-
-      startOver();
+        nextSequence();
+      }, 1000);
     }
-}
 
+  } else {
+
+    playSound("wrong");
+
+    $("body").addClass("game-over");
+    $("#level-title").text("Game Over! Score: " + level + " | High Score: " + highScore);
+
+    setTimeout(function () {
+      $("body").removeClass("game-over");
+    }, 200);
+
+    // Update high score
+    if (level > highScore) {
+      highScore = level;
+    }
+
+    startOver();
+  }
+}
 
 function nextSequence() {
   userClickedPattern = [];
   level++;
-  $("#level-title").text("Level " + level);
+
+  $("#level-title").text("Level " + level + " | High Score: " + highScore);
+
   var randomNumber = Math.floor(Math.random() * 4);
   var randomChosenColour = buttonColours[randomNumber];
   gamePattern.push(randomChosenColour);
 
   $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+
   playSound(randomChosenColour);
 }
 
 function animatePress(currentColor) {
   $("#" + currentColor).addClass("pressed");
+
   setTimeout(function () {
     $("#" + currentColor).removeClass("pressed");
   }, 100);
 }
 
+// ✅ FIXED SOUND PATH + CACHE ISSUE
 function playSound(name) {
-  var audio = new Audio(name + ".mp3");
-  audio.play();
+  var audio = new Audio("./" + name + ".mp3?v=2");
+  audio.play().catch(err => console.log("Audio error:", err));
 }
 
 function startOver() {
